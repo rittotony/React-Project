50653d868d480a530b39988fc53dec7b6a093c7a
Ritto Tomy <rittotony4@gmail.com>
2024-04-06 17:32:37 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <div className="App">
         <Navbar expand="lg" className="bg-body-tertiary">
           <Container>
-            <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand as={Link} to="/">CRUD</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,68 @@
-import React from "react";
-import './../App.css';
-
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
+import './Home.css';
 
 export const Home = () => {
-    return (
-        <div className="App">
-            <h3>This is Home page</h3>
-        </div>
-    )
-}
+ 
+    const [formData, setFormData] = useState({
+        fullName: "",
+        userEmail: "",
+        userPassword: "",
+        userPhone: ""
+      });
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+          ...formData,
+          [name]: value
+        });
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        // Access form data here (formData)
+        console.log(formData);
+        // You can perform further actions like sending data to server, etc.
+      };
+
+
+  return (
+    <div className="center">
+      <Card className="formCard">
+        <Card.Body>
+          <Card.Title className="cardTitle">Data collection</Card.Title>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="userFullName">
+              <Form.Label>Full name</Form.Label>
+              <Form.Control type="text" placeholder="Enter full name" name="fullName" value={FormData.fullName} onChange={handleChange} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="userEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" name="userEmail" value={FormData.userEmail} onChange={handleChange} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="userPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" name="userPassword" value={FormData.userPassword} onChange={handleChange} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="userPhone">
+              <Form.Label>Phone</Form.Label>
+              <Form.Control type="number" placeholder="Phone" name="userPhone" value={FormData.userPhone} onChange={handleChange} />
+            </Form.Group>
+
+            <Button variant="success" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      </div>
+  );
+};
+
+export default Home;
